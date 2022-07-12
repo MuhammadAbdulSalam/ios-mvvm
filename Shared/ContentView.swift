283b7524039d1227395b3ff3dcbd10a0d4b4fee0
Muhammad abdulsalam <muhammad.salam@nbrown.co.uk>
2022-07-12 14:22:19 +0100

    updated signin page design

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,9 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var username = "username"
+    @State private var password = "password"
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .center, spacing: 10){
+            
+            Spacer()
+            
+            Image("main-logo")
+                .resizable()
+                .frame(width: 150, height: 150)
+            
+            TextField( "username",text: $username)
+               .textInputAutocapitalization(.never)
+               .disableAutocorrection(true)
+               .frame(height: 40)
+               .textFieldStyle(.roundedBorder)
+
+
+            TextField( "password", text: $password)
+               .textInputAutocapitalization(.never)
+               .disableAutocorrection(true)
+               .frame(height: 40)
+               .textFieldStyle(.roundedBorder)
+
+            
+            Button("sign-in"){
+                print("sign-in clicked")
+            }
+            .frame(maxWidth: .infinity, alignment: .trailing)
+            .padding(.top, 10)
+            
+            
+            Spacer()
+            
+            Button("forgotten details? "){
+                print("forgotten details clicked")
+            }
+            
+        }.padding(.all, 20)
     }
 }
 
