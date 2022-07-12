94e93d1bcbcaf94ea022993fb5402ccfa0d8cbdc
Muhammad abdulsalam <muhammad.salam@nbrown.co.uk>
2022-07-12 08:31:10 +0100

    Initial Commit

---
+++ Shared/mvvmAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  mvvmAppApp.swift
+//  Shared
+//
+//  Created by adb_salam on 12/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct mvvmAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
